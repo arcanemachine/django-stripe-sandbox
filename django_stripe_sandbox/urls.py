b8ca4753fb27cd33d8b37ea819bb52cd080fa113
Nicholas Moen <arcanemachine@tutanota.com>
2021-09-11 15:40:18 -0600

    Remove unnecessary line of code

--- django_stripe_sandbox/urls.py
+++ django_stripe_sandbox/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from stripes import views as stripes_views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('stripes.urls')),
