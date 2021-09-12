92921ff0d4d532d18f87a5b83e4a2558872b0d36
Nicholas Moen <arcanemachine@tutanota.com>
2021-09-12 02:51:03 -0600

    Improve message description

--- scripts/runserver-test.sh
+++ scripts/runserver-test.sh
@@ -3,9 +3,9 @@
 stty sane  # prevent garbled text over ssh
 if [ $DJANGO_BASE_DIR ]
 then
-  python $DJANGO_BASE_DIR/manage.py testserver --addrport 0.0.0.0:7000 $DJANGO_BASE_DIR/cypress/fixtures/empty.json --noinput
+  python $DJANGO_BASE_DIR/manage.py testserver --addrport 0.0.0.0:7000 $DJANGO_BASE_DIR/cypress/fixtures/main.json --noinput
 else
   echo "DJANGO_BASE_DIR environment variable not specified. Using current directory..."
-  echo "This script should be run from the root folder of your Django app"
-  python $PWD/manage.py testserver --addrport 0.0.0.0:7000 $PWD/cypress/fixtures/empty.json --noinput
+  echo "\nUntil you have set a DJANGO_BASE_DIR environment variable, this script will need to be run from the root folder of your Django app"
+  python $PWD/manage.py testserver --addrport 0.0.0.0:7000 $PWD/cypress/fixtures/main.json --noinput
 fi
