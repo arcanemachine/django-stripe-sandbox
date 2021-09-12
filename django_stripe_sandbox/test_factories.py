65e7909440033d942df04d3d209969697ab5af95
Nicholas Moen <arcanemachine@tutanota.com>
2021-09-12 12:49:38 -0600

    Add simple test for factory

---
+++ django_stripe_sandbox/test_factories.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+from . import factories as f
+
+
+class CustomerFactoryTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_object = f.CustomerFactory()
+
+    def test_object_uses_expected_model(self):
+        self.assertEqual(self.test_object.__class__.__name__, 'Customer')
